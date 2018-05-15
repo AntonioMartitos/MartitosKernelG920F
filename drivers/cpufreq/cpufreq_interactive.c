4f65aeb26d751db0f795d17627bc546f146d4f87
AntonioMartitos <antonio.martos.mendez@gmail.com>
2018-05-15 19:52:46 +0200

    Tweak Interactive by Martitos 2

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -81,12 +81,12 @@ static spinlock_t regionchange_cpumask_lock;
 #endif
 
 /* Target load.  Lower values result in higher CPU speeds. */
-#define DEFAULT_TARGET_LOAD 93
+#define DEFAULT_TARGET_LOAD 99
 static unsigned int default_target_loads[] = {DEFAULT_TARGET_LOAD};
 
-#define DOWN_LOW_LOAD_THRESHOLD 7
+#define DOWN_LOW_LOAD_THRESHOLD 10
 
-#define DEFAULT_TIMER_RATE (20 * USEC_PER_MSEC)
+#define DEFAULT_TIMER_RATE (15 * USEC_PER_MSEC)
 #define DEFAULT_ABOVE_HISPEED_DELAY DEFAULT_TIMER_RATE
 static unsigned int default_above_hispeed_delay[] = {
 	DEFAULT_ABOVE_HISPEED_DELAY };
@@ -147,7 +147,7 @@ struct cpufreq_interactive_tunables {
 	 * The minimum amount of time to spend at a frequency before we can ramp
 	 * down.
 	 */
-#define DEFAULT_MIN_SAMPLE_TIME (40 * USEC_PER_MSEC)
+#define DEFAULT_MIN_SAMPLE_TIME (10 * USEC_PER_MSEC)
 	unsigned long min_sample_time;
 	/*
 	 * The sample rate of the timer used to increase frequency
